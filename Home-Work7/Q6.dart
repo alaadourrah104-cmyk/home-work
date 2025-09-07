19a7744603ffac18dd776404204f5a91ef9537dd
laa shraf <alaadourrah104@gmail.com>
2025-09-07 13:19:05 -0800

    done

--- Home-Work7/Q6.dart
+++ Home-Work7/Q6.dart
@@ -1,6 +1,23 @@
 // Q6. Number Guessing (3 Tries) - Generate a random number between 1 and 20. - Let the user
 // guess up to 3 times. If they fail, reveal the correct number.
 import 'dart:io';
+import 'dart:math';
 
-
-void main() {}
+void main() {
+  int number = Random().nextInt(20) + 1;
+  int tries = 3;
+  bool guess = false;
+  for (int i = 1; i < tries; i++) {
+    print("Enter  number ");
+    int guess = int.parse(stdin.readLineSync()!);
+    if (guess == number) {
+      print("You guessed correctly");
+      return;
+    } else if (guess < number) {
+      print("guess lower than number");
+    } else {
+      print("guess highter than number");
+    }
+  }
+  print("the correct number is $number");
+}
